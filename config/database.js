ceca75783b3afb58f431d959e18fb375c39f8ca1
Vonds <vonds13@gmail.com>
2018-11-18 09:03:15 -0500

    barrista app

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url': 'mongodb://milk:milk24@ds151753.mlab.com:51753/mango' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
